3e20e8008bd06da83852ec296524f8eb59e3a6f8
AnneLuciePuy <anne-lucie.puygrenier@open-groupe.com>
2023-11-20 09:48:53 +0100

    Deploying : deploying angular, adding post.

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -1,9 +1,12 @@
 const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+
+const jwtKey = process.env.JWT_Key || 'secret_this_should_be_longer';
 
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
-        const decodedToken = jwt.verify(token, process.env.JWT_Key);
+        const decodedToken = jwt.verify(token, jwtKey);
         req.userData = { email: decodedToken.email, userId: decodedToken.userId };
         next();
     } catch (error) {
